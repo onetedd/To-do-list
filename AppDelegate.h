a1cc79f4bd167aeedca4de38c28b73357063576f
Theodor Karlsson <Theodor.k.94@hotmail.com>
2013-11-18 14:10:21 +0100

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by T4 on 2013-11-18.
+//  Copyright (c) 2013 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
